7bc5e1c4da1f907435cfa1523025908720e675c6
maxgmarin <mgmarin@g.harvard.edu>
2020-06-12 09:46:35 -0400

    First full version of the Mtb RNA-seq pipeline 20/06/12

--- Mtb_RNAseq_SM_V1.py
+++ Mtb_RNAseq_SM_V1.py
@@ -11,22 +11,22 @@ refGenome_FA_PATH = config["RefGenome_FA_PATH"]
 refGenome_GFF_PATH = config["RefGenome_GFF_PATH"]
 
 
+Kraken2_DB_PATH = config["Kraken2_DB_PATH"]
+
+
 
 # Define PATH of OUTPUT directory
 MainDir = config["output_dir"]
 
 #output_Dir = MainDir + "sample_Output/"
-output_Dir = MainDir   # + "/"
+output_Dir = MainDir  + "/"
 
 
 import pandas as pd
 
 # Read in data regarding input 
-df = pd.read_csv( config["inputSampleData_TSV"], sep='\t')
+df = pd.read_csv( config["inputSampleData_TSV"], sep=',')
 
-
-# Create a python list of all input samples
-input_SampleIDs = list( df["SampleID"].values )
 
 # Save a list of SRA/ENA "Run" Accessions
 input_SampleIDs_SRA_RunAcc = list( df["Run"].values )
@@ -35,30 +35,31 @@ input_SampleIDs_SRA_RunAcc = list( df["Run"].values )
 
 rule all:
     input:
-        expand(f"{output_Dir}/{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}.fastq.gz", sampleID=input_SampleIDs_SRA_RunAcc),
+        expand(output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}.fastq.gz", sampleRunAcc=input_SampleIDs_SRA_RunAcc),
+        expand(output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FeatureCounts/{sampleRunAcc}.RNAseq.H37rv.ReadCounts.tsv", sampleRunAcc=input_SampleIDs_SRA_RunAcc),
+        expand(output_Dir + "{sampleRunAcc}/IlluminaRNAseq/fastqc_Trimmomatic/{sampleRunAcc}_fq_fastqc.html", sampleRunAcc=input_SampleIDs_SRA_RunAcc),
+        expand(output_Dir + "{sampleRunAcc}/IlluminaRNAseq/fastqc/{sampleRunAcc}_fq_fastqc.html", sampleRunAcc=input_SampleIDs_SRA_RunAcc),
 
 
-
-rule download_FQ-FromSRA_RunID:
+rule download_FQ_FromSRA_RunID:
     output: 
-        f"{output_Dir}/{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}.fastq.gz",
+        FQ_GZ = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}.fastq.gz",
     params:
-        outdir = f"{output_Dir}/{sampleRunAcc}/IlluminaRNAseq/FASTQs/"
+        target_DownloaDir = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FASTQs/"
+    conda:
+        "envs/sratools_2_10_7_Conda.yml"
     shell:
-        "fastq-dump --sra-id {wildcards.sampleRunAcc} --outdir {params.outdir} --gzip "
-
+        "fastq-dump --stdout {wildcards.sampleRunAcc} | gzip > {output.FQ_GZ} \n"
 
 
 
 rule fastqc_BeforeTrim:
     input:
-        f"{output_Dir}/{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}.fastq.gz",
+        output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}.fastq.gz",
     output:
         html = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/fastqc/{sampleRunAcc}_fq_fastqc.html",
         zip = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/fastqc/{sampleRunAcc}_fq_fastqc.zip" # the suffix _fastqc.zip is necessary for multiqc to find the file. If not using multiqc, you are free to choose an arbitrary filename
     params: ""
-    log:
-        output_Dir+"logs/fastqc/{sampleRunAcc}_fq.log"
     wrapper:
         "0.38.0/bio/fastqc"
 
@@ -69,12 +70,12 @@ rule fastqc_BeforeTrim:
 # can move adapter list to config file later
 rule trimmomatic_Illumina_SE_Trimming:
     input:
-         f"{output_Dir}/{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}.fastq.gz",
+         output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}.fastq.gz",
     output:
-        output_Dir + "/{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}_trimmed.fastq.gz",
+        output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}_trimmed.fastq.gz",
     params:
         # list of trimmers (see manual)
-        trimmer=["ILLUMINACLIP:'references/CustomTrimmoatic_IlluminaWGS_AdapterList.fasta':2:30:10:2:true SLIDINGWINDOW:4:20 MINLEN:75"],
+        trimmer=["ILLUMINACLIP:'references/CustomTrimmoatic_IlluminaWGS_AdapterList.fasta':2:30:10:2:true SLIDINGWINDOW:4:20 MINLEN:40"],
         compression_level="-9"
         # optional parameters
         # extra=" "
@@ -87,7 +88,7 @@ rule trimmomatic_Illumina_SE_Trimming:
 
 rule fastqc_OfTrimmomaticReads:
     input:
-        output_Dir + "/{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}_trimmed.fastq.gz",
+        output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}_trimmed.fastq.gz",
     output:
         html = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/fastqc_Trimmomatic/{sampleRunAcc}_fq_fastqc.html",
         zip = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/fastqc_Trimmomatic/{sampleRunAcc}_fq_fastqc.zip" # the suffix _fastqc.zip is necessary for multiqc to find the file. If not using multiqc, you are free to choose an arbitrary filename
@@ -98,10 +99,10 @@ rule fastqc_OfTrimmomaticReads:
 
 
 
-rule bwa_map_IllPE_AlignTo_H37rv:
+rule bwa_map_IllSE_AlignTo_H37rv:
     input:
-        H37rv_FA_PATH = refGenome_FA_PATH,
-        fq_trimmed = output_Dir + "/{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}_trimmed.fastq.gz",
+        refGenome_FA = refGenome_FA_PATH,
+        fq_trimmed = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}_trimmed.fastq.gz",
     output:
         output_Dir + "{sampleRunAcc}/IlluminaRNAseq/IlluminaSE_AlignedTo_H37rv/{sampleRunAcc}.IllSE.RNAseq.H37rv.sam"
     conda:
@@ -110,11 +111,7 @@ rule bwa_map_IllPE_AlignTo_H37rv:
         rg=r"@RG\tID:{sampleRunAcc}\tSM:{sampleRunAcc}"
     threads: 8
     shell:
-        "bwa mem -M -R '{params.rg}' -t {threads} {input.H37rv_FA_PATH} {input.fq_trimmed} > {output}"
-
-
-
-
+        "bwa mem -M -R '{params.rg}' -t {threads} {input.refGenome_FA} {input.fq_trimmed} > {output}"
 
 
 rule samtools_ViewAndSort_IllPE_AlignTo_H37rv:
@@ -129,11 +126,11 @@ rule samtools_ViewAndSort_IllPE_AlignTo_H37rv:
 
 
 
-rule samtools_index_IllPE_AlignTo_H37rv:
+rule samtools_index_IllSE_AlignTo_H37rv:
     input:
         output_Dir + "{sampleRunAcc}/IlluminaRNAseq/IlluminaSE_AlignedTo_H37rv/{sampleRunAcc}.IllSE.RNAseq.H37rv.bam"
     output:
-        output_Dir+ "{sampleRunAcc}/IlluminaRNAseq/IlluminaSE_AlignedTo_H37rv/{sampleRunAcc}.IllPE.H37rv.bam.bai"
+        output_Dir+ "{sampleRunAcc}/IlluminaRNAseq/IlluminaSE_AlignedTo_H37rv/{sampleRunAcc}.IllSE.RNAseq.H37rv.bam.bai"
     conda:
         "envs/IlluminaPE_Processing_Conda.yml"
     shell:
@@ -143,19 +140,61 @@ rule samtools_index_IllPE_AlignTo_H37rv:
 
 rule featureCounts_H37rv_GFF:
     input:
-        ""
+        RNAseq_ToH37rv_BAM = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/IlluminaSE_AlignedTo_H37rv/{sampleRunAcc}.IllSE.RNAseq.H37rv.bam",
+        RNAseq_ToH37rv_BAI = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/IlluminaSE_AlignedTo_H37rv/{sampleRunAcc}.IllSE.RNAseq.H37rv.bam.bai", 
+        refGenome_GFF = refGenome_GFF_PATH,
     output:
-        ""
+        ReadCountsMatrix = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FeatureCounts/{sampleRunAcc}.RNAseq.H37rv.ReadCounts.tsv",
+        FeatureCounts_Summary = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FeatureCounts/{sampleRunAcc}.RNAseq.H37rv.ReadCounts.tsv.summary"
     conda:
-        ""
+        "envs/subread_featurecounts_2_0_1_Conda.yml"
     shell:
-        "featureCounts -t gene -g locus_tag -a {refGTF} -o {output.ReadCountsMatrix} {input.RNAseq_To_H37rv_BAM}"
+        "featureCounts -t gene -g locus_tag -a {input.refGenome_GFF} -o {output.ReadCountsMatrix} {input.RNAseq_ToH37rv_BAM}"
 
 
+rule Kraken2_Illumina_SE:
+    input:
+        Kraken2_DB = Kraken2_DB_PATH,
+        fq1_trimmed = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FASTQs/{sampleRunAcc}.fastq.gz",
+    output:
+        ReadClassifications = output_Dir + "{sampleID_WiIll}/IlluminaRNAseq/Kraken2/{sampleID_WiIll}.Kraken2.Output.Reads.txt",
+        Kraken2_Report = output_Dir + "{sampleID_WiIll}/IlluminaRNAseq/Kraken2/{sampleID_WiIll}.Kraken2.Output.Report.txt"
+    conda:
+        "envs/kraken2_2_0_8_Conda.yml"
+    threads: 1
+    shell:
+        " kraken2 --use-names --threads {threads} --db {input.Kraken2_DB} --output {output.ReadClassifications} "
+        " --report {output.Kraken2_Report} {input.fq1_trimmed}   "
 
 
+"""
+rule Filter_FeatureCountsOutput_ReadCountsOnly:
+    input:
+        FeatureCounts_OutputFile = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FeatureCounts/{sampleRunAcc}.RNAseq.H37rv.ReadCounts.tsv",
+    output:
+        ReadCounts_Col7_Only_TSV = output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FeatureCounts/{sampleRunAcc}.RNAseq.H37rv.ReadCounts.Col7_Only.tsv",
+    shell:
+        "cat {input} | sed '1d' | cut -f 7 > {output}"
 
 
+rule get_GeneIDs_FromFeatureCounts_Output:
+    input:
+        expand(output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FeatureCounts/{sampleRunAcc}.RNAseq.H37rv.ReadCounts.Col7_Only.tsv", sampleRunAcc=input_SampleIDs_SRA_RunAcc),
+    output: 
+        geneIDs_TXT = output_Dir + "FeatureCounts_MergedReadCounts/geneids.txt",
+    shell:
+        "ls -1 {input} | head -1 | xargs cut -f 1 > {output} "
+
+
+rule merge_All_FeatureCounts_ToMatrix:
+    input:
+        geneIDs_TXT = output_Dir + "FeatureCounts_MergedReadCounts/geneids.txt",
+        setOfAll_CountsFiles = expand(output_Dir + "{sampleRunAcc}/IlluminaRNAseq/FeatureCounts/{sampleRunAcc}.RNAseq.H37rv.ReadCounts.Col7_Only.tsv", sampleRunAcc=input_SampleIDs_SRA_RunAcc),
+    output: 
+        FeatureCounts_ReadsCountMatrix_Merged = output_Dir + "FeatureCounts_MergedReadCounts/FeatureCounts.ReadsCountMatrix.Merged.tsv",
+    shell:
+        "paste {input.geneIDs_TXT} {input.setOfAll_CountsFiles} > {output.FeatureCounts_ReadsCountMatrix_Merged}"
+"""
 
 
 
